cbed3e6a455303c5c94beb5152a67d4710c8b55e
Denisse Alfaro <dalfaro@konradgroup.com>
2021-01-01 20:14:38 -0600

    feat: Display a list section

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,9 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES; //Heroes array 
+  selectedHero: Hero; //Property of type hero
 
   hero: Hero = {
     id: 1,
@@ -16,6 +20,14 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Sets the property to the hero object that was clicked on
+   * @param hero The selected hero
+   */
+  onSelect(hero: Hero): void { 
+    this.selectedHero = hero;
   }
 
 }
